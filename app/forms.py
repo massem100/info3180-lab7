e8b826f0e32fe4af171b4850ccaf4f88bcfc1ec5
Krystall Messam <massem100@outlook.com>
2020-04-20 08:25:51 -0500

    Flask Form created

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import  TextAreaField
+from wtforms.validators import InputRequired, Email
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[InputRequired(message='A description is required.')])
+    photo = FileField('Photo', validators=[FileRequired('No photo was submitted.'), FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
