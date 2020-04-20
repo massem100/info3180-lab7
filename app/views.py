3b453bfa1d2ed77953615d20f215f510b76cd794
Krystall Messam <massem100@outlook.com>
2020-04-20 08:45:27 -0500

    Route and View Created

--- app/views.py
+++ app/views.py
@@ -4,13 +4,32 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import os
 from app import app
-from flask import render_template, request
-
+from flask import render_template, request, jsonify
+from app.forms import UploadForm
+from werkzeug.utils import secure_filename
 ###
 # Routing for your application.
 ###
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    form = UploadForm()
+    if request.method == "POST" and form.validate_on_submit():
+
+        description = form.description.data
+        photo = form.photo.data
+
+        photo_filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'], photo_filename))
+
+        data= [{'message': 'File Upload Successful', 'filename': photo_filename, 'description': description}]
+        return jsonify(data)
+    else:
+        error_list = form_errors(form)
+        error = [{'errors': error_list}]
+        return  jsonify(error)
+
 
 
 # Please create all new routes and view functions above this route.
